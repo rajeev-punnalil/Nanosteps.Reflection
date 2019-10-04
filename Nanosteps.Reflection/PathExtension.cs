bb0b5c54275a20eb65f4c9d9ff3bcf8756f6b382
Rajeev Punnalil <rajeev.punnalil@hotmail.com>
2019-10-04 22:56:45 +0530

    Reflection path extension methods added

---
+++ Nanosteps.Reflection/PathExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Nanosteps.Reflection
+{
+    /// <summary>
+    /// Extend path functions related with reflections
+    /// </summary>
+    public static partial class ReflectionAssemblyExtensions
+    {
+        /// <summary>
+        /// Get the location of the entry assembly
+        /// </summary>
+        /// <returns></returns>
+        public static DirectoryInfo GetEntryLocation()
+        {
+            return new DirectoryInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Parent;
+        }
+
+        /// <summary>
+        /// Get directory related to path
+        /// </summary>
+        /// <param name="relativePath"></param>
+        /// <returns></returns>
+        public static DirectoryInfo GetEntyRelativeLocation(string relativePath)
+        {
+            return new DirectoryInfo(Path.Combine(GetEntryLocation().FullName, relativePath));
+        }
+
+        /// <summary>
+        /// Get entry relative file
+        /// </summary>
+        /// <param name="relativePath"></param>
+        /// <returns></returns>
+        public static FileInfo GetEntyRelativeFile(string relativePath)
+        {
+            return new FileInfo(Path.Combine(GetEntryLocation().FullName, relativePath));
+        }
+    }
+}
--- Reflection.Tests/VersionEExtesnionTests.cs
+++ Reflection.Tests/VersionEExtesnionTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nanosteps.Reflection;
+using System.IO;
 using System.Reflection;
 
 namespace Reflection.Tests
@@ -21,6 +22,30 @@ namespace Reflection.Tests
             Assert.IsTrue(!string.IsNullOrWhiteSpace(version));
         }
 
+        [TestMethod]
+        public void GetEntryLocation_01()
+        {
+            var location = ReflectionAssemblyExtensions.GetEntryLocation(); 
+            Assert.IsTrue(new DirectoryInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Parent.FullName == location.FullName);
+        }
+
+        [TestMethod]
+        public void GetEntyRelativeLocation_01()
+        {
+            var location = ReflectionAssemblyExtensions.GetEntryLocation();
+            var subDir = location.CreateSubdirectory("Test");
+            var testFolder = ReflectionAssemblyExtensions.GetEntyRelativeLocation("Test");
+            Assert.IsTrue(subDir.FullName == testFolder.FullName);
+        }
+
+        [TestMethod]
+        public void GetEntyRelativeFile_01()
+        {
+            var location = ReflectionAssemblyExtensions.GetEntryLocation();
+            var targetFile = new FileInfo(Path.Combine(location.FullName, "test.txt"));
+            var finalFile  = ReflectionAssemblyExtensions.GetEntyRelativeFile("test.txt");
+            Assert.IsTrue(targetFile.FullName == targetFile.FullName);
+        }
     }
 }
 
